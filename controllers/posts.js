a442079f509fa8628e5c2d1aead41ad4ad619f10
DominicBarnett <dominic.barnett@icloud.com>
2024-04-17 13:28:17 -0700

    users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,13 @@ const Post = require('../models/post');
 
 module.exports = (app) => {
 
+    app.get('/', (req, res) => {
+        Post.find({}).lean()
+          .then((posts) => res.render('posts-index', { posts }))
+          .catch((err) => {
+            console.log(err.message);
+          })
+      });
   // CREATE
   app.post('/posts/new', (req, res) => {
     // INSTANTIATE INSTANCE OF POST MODEL
@@ -10,14 +17,18 @@ module.exports = (app) => {
     // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
     post.save(() => res.redirect('/'));
   });
+  app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+  });
 
-  app.get('/', (req, res) => {
-    Post.find({}).lean()
-      .then((posts) => res.render('posts-index', { posts }))
-      .catch((err) => {
-        console.log(err.message);
-      })
-  })
+//   LOOK UP THE POST
+    app.get('/posts/:id', (req, res) => {
+        Post.findById(req.params.id).lean()
+        .then((post) => res.render('posts-show', { post }))
+        .catch((err) => {
+            console.log(err.message);
+        });
+    });
 
 };
 
--- server.js
+++ server.js
@@ -1,36 +1,27 @@
-// Initialize express
-const express = require('express')
-const app = express()
-// require handlebars
-const exphbs = require('express-handlebars');
-const Handlebars = require('handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Use "main" as our default layout
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
-// Use handlebars to render
+// Set up Handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ defaultLayout: 'main' });
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Required
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-require('./controllers/posts')(app);
 
 // Set db
 require('./data/reddit-db');
 
-// Render the "home" layout for the main page and send the following msg
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-  })
+// Posts Controller
+require('./controllers/posts')(app);
 
-app.get('/posts/new', (req, res) => {
-  res.render('posts-new');
-})
-// Choose a port to listen on
-const port = process.env.PORT || 3000;
-
-// Tell the app what port to listen on
+// Start the server
 app.listen(port, () => {
-  console.log('App listening on port 3000!')
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+module.exports = app;
