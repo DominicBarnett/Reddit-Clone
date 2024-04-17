9e7d91c7cad004642832280f727c21bc3e6929ba
DominicBarnett <dominic.barnett@icloud.com>
2024-04-17 10:17:58 -0700

    users can submit and save posts

--- server.js
+++ server.js
@@ -11,11 +11,22 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allo
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+require('./controllers/posts')(app);
+
+// Set db
+require('./data/reddit-db');
+
 // Render the "home" layout for the main page and send the following msg
 app.get('/', (req, res) => {
     res.render('home', { msg: 'Handlebars are Cool!' });
   })
 
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new');
+})
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
 
