82a9f38c2d9d1d0c5819ec2a4061b7192eb7973c
DominicBarnett <dominic.barnett@icloud.com>
2024-04-19 12:26:19 -0700

    users can comment

---
+++ controllers/comments.js
@@ -0,0 +1,19 @@
+require('./controllers/comments.js')(app);
+const Comment = require('../models/comment');
+
+module.exports = (app) => {
+// CREATE Comment
+app.post('/posts/:postId/comments', (req, res) => {
+    // INSTANTIATE INSTANCE OF MODEL
+    const comment = new Comment(req.body);
+  
+    // SAVE INSTANCE OF Comment MODEL TO DB
+    comment
+      .save()
+      // REDIRECT TO THE ROOT
+      .then(() => res.redirect('/'))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports = (app) => {
 
@@ -23,16 +24,34 @@ module.exports = (app) => {
 
 //   LOOK UP THE POST
     app.get('/posts/:id', (req, res) => {
-        Post.findById(req.params.id).lean()
+        Post.findById(req.params.id).lean().populate('comments')
         .then((post) => res.render('posts-show', { post }))
         .catch((err) => {
-            console.log(err.message);
-        });
+    console.log(err.message);
+  });
     });
 // SUBREDDIT
 app.get('/n/:subreddit', (req, res) => {
     Post.find({ subreddit: req.params.subreddit }).lean()
       .then((posts) => res.render('posts-index', { posts }))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  // CREATE Comment
+app.post('/posts/:postId/comments', (req, res) => {
+    // INSTANTIATE INSTANCE OF MODEL
+    const comment = new Comment(req.body);
+  
+    // SAVE INSTANCE OF Comment MODEL TO DB
+    comment
+      .save()
+      .then(() => Post.findById(req.params.postId))
+      .then((post) => {
+        post.comments.unshift(comment);
+        return post.save();
+      })
+      .then(() => res.redirect('/'))
       .catch((err) => {
         console.log(err);
       });
