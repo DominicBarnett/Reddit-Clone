7e56b9deae33e0f106020b5f69e31d6d6564a306
DominicBarnett <dominic.barnett@icloud.com>
2024-04-20 17:04:46 -0700

    users can sign up

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Set up Handlebars
+require('dotenv').config();
+// Initialize express
+const express = require('express')
+const app = express()
+// require handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ defaultLayout: 'main' });
-app.engine('handlebars', hbs.engine);
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+// Use handlebars to render
 app.set('view engine', 'handlebars');
 
-// Required
-app.use(express.urlencoded({ extended: true }));
+// Security
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+app.use(cookieParser());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+require('./controllers/posts')(app);
+require('./controllers/auth.js')(app);
 
 
 // Set db
 require('./data/reddit-db');
 
-// Posts Controller
-require('./controllers/posts')(app);
+// Render the "home" layout for the main page and send the following msg
+app.get('/', (req, res) => {
+    res.render('home', { msg: 'Handlebars are Cool!' });
+  })
 
-// Start the server
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new');
+})
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
-module.exports = app;
+  console.log('App listening on port 3000!')
+})
