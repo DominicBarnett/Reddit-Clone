c8c9178bcf4292ab0e23770d65663e6c1f304686
DominicBarnett <dominic.barnett@icloud.com>
2024-04-22 08:10:25 -0700

    authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,23 +1,38 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 const Comment = require('../models/comment');
 
 module.exports = (app) => {
 
-    app.get('/', (req, res) => {
-        const currentUser = req.user;
-      
-        Post.find({})
-          .then((posts) => res.render('posts-index', { posts, currentUser }))
-          .catch((err) => {
-            console.log(err.message);
-          });
-      });
-// CREATE
-app.post('/posts/new', (req, res) => {
+// INDEX
+app.get('/', (req, res) => {
+  const { user } = req;
+  console.log(req.cookies);
+  Post.find({}).lean().populate('author')
+    .then((posts) => res.render('posts-index', { posts, user }))
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+  // CREATE
+  app.post('/posts/new', (req, res) => {
     if (req.user) {
+      const userId = req.user._id;
       const post = new Post(req.body);
-  
-      post.save(() => res.redirect('/'));
+      post.author = userId;
+
+      post
+        .save()
+        .then(() => User.findById(userId))
+        .then((user) => {
+          user.posts.unshift(post);
+          user.save();
+          // REDIRECT TO THE NEW POST
+          return res.redirect(`/posts/${post._id}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
     } else {
       return res.status(401); // UNAUTHORIZED
     }
@@ -26,22 +41,26 @@ app.post('/posts/new', (req, res) => {
     res.render('posts-new')
   });
 
-//   LOOK UP THE POST
-    app.get('/posts/:id', (req, res) => {
-        Post.findById(req.params.id).lean().populate('comments')
-        .then((post) => res.render('posts-show', { post }))
-        .catch((err) => {
-    console.log(err.message);
-  });
+// LOOK UP THE POST
+app.get('/posts/:id', (req, res) => {
+  const currentUser = req.user;
+
+  Post.findById(req.params.id).lean().populate('comments').populate('author')
+    .then((post) => res.render('posts-show', { post, currentUser }))
+    .catch((err) => {
+      console.log(err.message);
     });
+});
 // SUBREDDIT
 app.get('/n/:subreddit', (req, res) => {
-    Post.find({ subreddit: req.params.subreddit }).lean()
-      .then((posts) => res.render('posts-index', { posts }))
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+  const currentUser = req.user;
+  const { subreddit } = req.params;
+  Post.find({ subreddit }).lean().populate('author')
+    .then((posts) => res.render('posts-index', { posts, currentUser }))
+    .catch((err) => {
+      console.log(err);
+    });
+});
   // CREATE Comment
 app.post('/posts/:postId/comments', (req, res) => {
     // INSTANTIATE INSTANCE OF MODEL
